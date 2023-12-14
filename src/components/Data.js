a103345c93ddca719b17742fe0c2b18428ab1c37
vishu270422 <129824992+vishu270422@users.noreply.github.com>
2023-12-14 20:13:24 +0530

    Add files via upload

---
+++ src/components/Data.js
@@ -0,0 +1,43 @@
+export const QuizData = [
+    {
+        question: "What is the 2+2+2?",
+        options:[ "8", "3", "5", "6"],
+        answer: 4
+    },
+    {
+        question: "What is the 10*6",
+        options:["40", "60", "66", "69"],
+        answer: 2
+    },
+    {
+        question: "What is 100/2?",
+        options: ["50", "100", "51", "55"],
+        answer: 1
+    },
+    {
+        question: "What is the 2+9?",
+        options: ["12", "11", "13", "19"],
+        answer:2
+    },
+
+    {
+        question: "What is the 2+22+2?",
+        options:[ "28", "23", "25", "26"],
+        answer: 4
+    },
+    {
+        question: "What is the 20*2",
+        options:["40", "20", "26", "29"],
+        answer: 2
+    },
+    {
+        question: "What is 100/4?",
+        options: ["25", "50", "51", "55"],
+        answer: 1
+    },
+    {
+        question: "What is the 21+19?",
+        options: ["22", "40", "43", "39"],
+        answer:2
+    },
+];
